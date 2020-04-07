c414614ab752e1f7d77596c1a03e6f225fc37f14
Paige Baily <pbaily@uncc.edu>
2020-04-07 17:27:18 +0000

    Added validation and error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,37 @@
 class ArticlesController < ApplicationController
     def new
+      @article = Article.new
+    end
+    def index
+      @articles=Article.all 
     end
     def show
-      @article = Article.find(params[:id])
+      @article =Article.find(params[:id])
+   end
+  def create
+  @article =Article.new(article_params)
+  if @article.save
+    redirect_to @article
+    else
+    render'new'
     end
-def create
-  @article = Article.new(article_params)
+  end
+def edit
+  @article = Article.edit(find(params[:id]))
   @article.save
   redirect_to @article
-  
+end
+def update
+@article = Article.find(params[:id])
+if @article.update(article_params)
+redirect_to @article
+else
+render 'edit'
 end
 end
+end
+
 private 
 def article_params
-  params.require(:article).permit(:titlw, :text)
+  params.require(params[:id]).permit(:title, :text)
 end
